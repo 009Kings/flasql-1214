7c5f3060954e3b133443337adb4e36caa579f9f7
009kings <009kings@gmail.com>
2021-03-03 16:00:03 -0800

    full CRUD

--- api.py
+++ api.py
@@ -1,5 +1,5 @@
 from models import app, db, User
-from flask import jsonify, request
+from flask import jsonify, request, redirect, url_for
 
 @app.route("/")
 def home():
@@ -20,6 +20,37 @@ def user_index_create():
     db.session.commit()
     print(new_user)
     return jsonify(new_user.to_dict())
+
+@app.route("/users/<int:id>", methods=["GET", "PUT", "DELETE"])
+def user_show_update_delete(id):
+  if request.method == "GET":
+    # get a user
+    user = User.query.get(id)
+    if user:
+      return jsonify(user.to_dict())
+    else:
+      return jsonify(message="There is no user here")
+  if request.method == "PUT":
+    # get a user
+    user = User.query.get(id)
+    if user:
+      # update user
+      for key, value in request.form.items():
+        print(f"💥 {key}: {value}")
+        setattr(user, key, value)
+      db.session.commit()
+      return jsonify(user.to_dict())
+    else: 
+      return jsonify(message="There is no user here")
+  if request.method == "DELETE":
+    # delete a user
+    user = User.query.get(id)
+    if user:
+      db.session.delete(user)
+      db.session.commit()
+      return jsonify(message="successful deletion")
+    else:
+      return jsonify(message="no user here")
 
 if __name__ == "__main__":
   app.run()
